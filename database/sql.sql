ab01b11016c92107e8d9cc4b88693182bf036c3d
Luis Iñigo <luisvib2003@gmail.com>
2023-02-22 11:18:48 +0100

    likes y dislikes en funcionamiento

--- database/sql.sql
+++ database/sql.sql
@@ -12,6 +12,6 @@ CREATE TABLE FOTOS(
     fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO FOTOS(titulo,url,descripcion) VALUES("Paisajes","/images/imagen1.jpeg","Esto es un paisaje muy bonito");
-INSERT INTO FOTOS(titulo,url,descripcion) VALUES("Tigre","/images/imagen2.jpeg","Esto es un tigre muy bonito");
-INSERT INTO FOTOS(titulo,url,descripcion) VALUES("Elefante","/images/imagen3.jpeg","Esto es un elefante muy bonito");
+INSERT INTO FOTOS(titulo,url,descripcion,likes, dislikes)  VALUES("Paisajes","/images/imagen1.jpeg","Esto es un paisaje muy bonito",0,0);
+INSERT INTO FOTOS(titulo,url,descripcion,likes, dislikes) VALUES("Tigre","/images/imagen2.jpeg","Esto es un tigre muy bonito",0,0);
+INSERT INTO FOTOS(titulo,url,descripcion,likes, dislikes)  VALUES("Elefante","/images/imagen3.jpeg","Esto es un elefante muy bonito",0,0);
--- routes/index.js
+++ routes/index.js
@@ -14,19 +14,20 @@ router.get("/fotos", async (req,res) =>{
 
 router.post("/subirfoto", async (req, res) =>{
   const {titulo, url, descripcion} = req.body
-  await pool.query(`INSERT INTO FOTOS(titulo, url, descripcion) VALUES('${titulo}', '${url}', '${descripcion}')`)
+  await pool.query(`INSERT INTO FOTOS(titulo, url, descripcion, likes, dislikes) VALUES('${titulo}', '${url}', '${descripcion}, 0, 0')`)
   res.redirect("/")
 })
 
 router.get("/like/:id", async (req, res) =>{
   const {id} = req.params
-  await pool.query(`UPDATE FOTOS`)
-  res.send(id)
+  await pool.query(`UPDATE FOTOS SET likes=likes+1 WHERE id=${id}`)
+  res.redirect("/fotos")
 })
 
-router.get("/dislike/:id", (req, res) =>{
+router.get("/dislike/:id", async (req, res) =>{
   const {id} = req.params
-  res.send(id)
+  await pool.query(`UPDATE FOTOS SET dislikes=dislikes+1 WHERE id=${id}`)
+  res.redirect("/fotos")
 })
 
 module.exports = router;
